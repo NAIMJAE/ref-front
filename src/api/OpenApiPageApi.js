59319ed1d61776aedddb912159ee399c2cbef3c4
NAIMJAE <skdlawo5985@gamil.com>
2024-08-22 18:38:25 +0900

    2

--- src/api/OpenApiPageApi.js
+++ src/api/OpenApiPageApi.js
@@ -14,13 +14,13 @@ export const selectServiceKeyApi = async (uid) => {
     return response.data;
 };
 
-// serviceKey 조회
+// 이름으로 데이터 조회
 export const getInfoByNameApi = async (data) => {
     const response = await axios.get(`${rootURL}/shinchan/v1/getCharacterInfo?serviceKey=${data.sk}&type=${data.type}&name=${data.name}`);
     return response.data;
 };
 
-// 조회
+// 캐릭터 정보 리스트 조회
 export const getAllInfoApi = async (data) => {
     const response = await axios.get(`${rootURL}/shinchan/v1/getCharacterListInfo?serviceKey=${data.sk}&type=${data.type}&numOfRows=${data.numOfRows}&pageNo=${data.pageNo}`);
     return response.data;
