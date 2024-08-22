ef3e37c5b653bd9160ce05f2a71e1504760d5edd
linma21 <154877422+linma21@users.noreply.github.com>
2024-08-22 19:25:54 +0900

    게임 구현중...

--- src/pages/main/MainPage.js
+++ src/pages/main/MainPage.js
@@ -18,6 +18,11 @@ const MainPage = () => {
           <h1>Open API 제작</h1>
           <h2>Open API를 활용한 예제</h2>
         </div>
+
+        <div className='refBox' onClick={() => navigate('/tictactoe')}>
+          <h1>Tic-tac-toe</h1>
+          <h2>Socket을 활용한 게임 예제</h2>
+        </div>
       </div>
     </MainLayout>
   )
--- src/router/root.js
+++ src/router/root.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import MainPage from "../pages/main/MainPage";
 import ApiPage from "../pages/API/ApiPage";
 import OpenApiPage from "../pages/API/OpenApiPage";
+import Tictactoe from "../pages/game/Tictactoe";
 
 const root = createBrowserRouter([
     // main
@@ -10,5 +11,8 @@ const root = createBrowserRouter([
     // API
     { path: '/api', element: <ApiPage /> },
     { path: '/openApi', element: <OpenApiPage /> },
+
+    // Game
+    { path: '/tictactoe', element: <Tictactoe /> },
 ]);
 export default root;
