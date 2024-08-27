d06299bb5b6892282815ef39f4ab5bcf15d11416
linma21 <154877422+linma21@users.noreply.github.com>
2024-08-27 19:06:57 +0900

    살려줘

--- src/pages/game/Tictactoe.js
+++ src/pages/game/Tictactoe.js
@@ -2,69 +2,28 @@ import React, { useEffect, useRef, useState } from 'react'
 import MainLayout from '../../layout/MainLayout'
 import '../../styles/game.scss'
 import { Host } from "../../api/RootUrl";
+import TictactoeComponent from '../../component/TictactoeComponent';
 
 const Tictactoe = () => {
-    /**  내 차례인지 확인하는 변수 */
-    const [myTurn, setMyTurn] = useState(true);
+    const [selectedRoomId, setSelectedRoomId] = useState(null);
 
-    /** 현재 플레이어의 기호 (X 또는 O) */
-    const [playerSymbol, setPlayerSymbol] = useState("O");
+    /** 게임방 입장 */
+    const enterRoom = (roomId) => {
+        setSelectedRoomId(roomId);
+    };
 
-    /** 게임 보드 2차원 배열  */
-    const [board, setBoard] = useState([
-        [null, null, null],
-        [null, null, null],
-        [null, null, null],
-    ]);
-
-    const webSocket = useRef();
-
-    useEffect(() => {
-        /** 소켓 연결 */
-        webSocket.current = new WebSocket(`ws://${Host}:8080/tictactoe`);
-
-        webSocket.current.onopen = () => {
-            console.log('WebSocket 연결!');
-        };
-        webSocket.current.onclose = (error) => {
-            console.log(error);
-        }
-        webSocket.current.onerror = (error) => {
-            console.log(error);
-        }
-
-    }, [])
-
-    /** 버튼 클릭 */
-    const clickHandler = (rowIndex, colIndex) => {
-        // 이미 클릭된 버튼은 무시
-        if (board[rowIndex][colIndex] !== null) return;
-
-        // 업데이트를 위한 새 배열 
-        const newBoard = board.map((row, rIndex) =>
-            row.map((cell, cIndex) =>
-                // 해당 버튼의 인덱스 값만 변경 
-                rIndex === rowIndex && cIndex === colIndex ? playerSymbol : cell
-            )
-        );
-
-        // 새로운 보드 상태로 업데이트
-        setBoard(newBoard);
-
-        setMyTurn(!myTurn);
-
-        // 게임 종료 조건 확인 
-    }
     return (
         <MainLayout>
-            <div id='Tictactoe'>
-                {board.map((row, rowIndex) => (
-                    <div key={rowIndex} className='ctnRow'>
-                        {row.map((cell, colIndex) => (
-                            <button key={colIndex} className={`btnTictactoe ${board[rowIndex][colIndex] === 'O' ? 'circle' : board[rowIndex][colIndex] === 'X' ? 'cross' : ''}`} onClick={() => clickHandler(rowIndex, colIndex)}></button>
-                        ))}
-                    </div>
-                ))}
+            <div className='colDiv'>
+            <div className='roomList'>
+                    {[1, 2, 3].map(roomId => (
+                        <div key={roomId} className='room' onClick={() => enterRoom(roomId)}>
+                            <h1>{roomId}번 방</h1>
+                            <h2>현재 인원 / 2</h2>
+                        </div>
+                    ))}
+                </div>
+                {selectedRoomId && <TictactoeComponent roomId={selectedRoomId} />}
             </div>
         </MainLayout>
     )
