3fb2e526869191393e252e1829639601eb0df579
NAIMJAE <skdlawo5985@gamil.com>
2024-10-26 23:42:53 +0900

    visitorTracking UI update

--- src/api/RootUrl.js
+++ src/api/RootUrl.js
@@ -2,8 +2,8 @@
 export const Host = 'localhost:8080';
 export const RootUrl = () => {
     //return `https://refcode.shop/ref`;
-    //return `https://api.refcode.info/ref`;
-    return `http://localhost:8080/ref`;
+    return `https://api.refcode.info/ref`;
+    //return `http://localhost:8080/ref`;
 };
 
 export const FrontUrl = () => {
